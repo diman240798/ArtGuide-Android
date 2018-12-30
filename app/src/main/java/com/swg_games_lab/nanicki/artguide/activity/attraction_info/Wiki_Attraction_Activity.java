95e36f7036fe617b29a607c58254603eea0283f7
Dmitriy <diman240798@yandex.ru>
2018-12-30 23:20:14 +0300

    Add Speaker animation into Wiki_Attraction_Activity. Start workking on TextToSpeach

--- app/src/main/java/com/swg_games_lab/nanicki/artguide/activity/attraction_info/Wiki_Attraction_Activity.java
+++ app/src/main/java/com/swg_games_lab/nanicki/artguide/activity/attraction_info/Wiki_Attraction_Activity.java
@@ -1,5 +1,6 @@
 package com.swg_games_lab.nanicki.artguide.activity.attraction_info;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,11 +9,21 @@ import android.widget.TextView;
 
 import com.swg_games_lab.nanicki.artguide.R;
 
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 public class Wiki_Attraction_Activity extends AppCompatActivity {
 
     ImageView imageView;
     TextView titleTW, descrTW;
     Button listenBTN;
+    private GifImageView guideSpeaker;
+    private GifDrawable gifFromResource;
+    private Drawable playImage;
+    private Drawable pauseImage;
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -23,6 +34,30 @@ public class Wiki_Attraction_Activity extends AppCompatActivity {
         descrTW = (TextView) findViewById(R.id.wiki_attr_descriptionTW);
         listenBTN = (Button) findViewById(R.id.wiki_attr_listenBTN);
 
+        playImage = getResources().getDrawable(R.drawable.listen_play);
+        pauseImage = getResources().getDrawable(R.drawable.listen_pause);
+
+
+        ///////////
+        guideSpeaker = (GifImageView) findViewById(R.id.wiki_attr_speaking_heroIV);
+        try {
+            gifFromResource = new GifDrawable( getResources(), R.drawable.gif_speaking_hero);
+            gifFromResource.stop();
+            guideSpeaker.setImageDrawable(gifFromResource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        listenBTN.setOnClickListener(v-> {
+            boolean running = gifFromResource.isRunning();
+            if (running) {
+                gifFromResource.pause();
+                listenBTN.setCompoundDrawablesWithIntrinsicBounds(playImage, null, null, null);
+            } else {
+                gifFromResource.start();
+                listenBTN.setCompoundDrawablesWithIntrinsicBounds(pauseImage, null, null, null);
+            }
+        });
 
         if (bundle == null) {
             Bundle extras = getIntent().getExtras();
