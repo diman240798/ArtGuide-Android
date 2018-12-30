34f6b90587ec0cf1e9f29d93a5ab15d4237d97e5
Dmitriy <diman240798@yandex.ru>
2018-12-31 00:11:46 +0300

    textToSpeech added

--- app/src/main/java/com/swg_games_lab/nanicki/artguide/activity/attraction_info/Wiki_Attraction_Activity.java
+++ app/src/main/java/com/swg_games_lab/nanicki/artguide/activity/attraction_info/Wiki_Attraction_Activity.java
@@ -1,6 +1,8 @@
 package com.swg_games_lab.nanicki.artguide.activity.attraction_info;
 
 import android.graphics.drawable.Drawable;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 import com.swg_games_lab.nanicki.artguide.R;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Set;
 
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
@@ -23,6 +27,7 @@ public class Wiki_Attraction_Activity extends AppCompatActivity {
     private GifDrawable gifFromResource;
     private Drawable playImage;
     private Drawable pauseImage;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -37,6 +42,16 @@ public class Wiki_Attraction_Activity extends AppCompatActivity {
         playImage = getResources().getDrawable(R.drawable.listen_play);
         pauseImage = getResources().getDrawable(R.drawable.listen_pause);
 
+        textToSpeech = new TextToSpeech(this, status -> {
+            if (status == TextToSpeech.SUCCESS) {
+                textToSpeech.setLanguage(new Locale("ru"));
+                Set<Voice> voices = textToSpeech.getVoices();
+                for (Voice voice : voices) {
+                    if (voice.getName().equals("ru-ru-x-dfc#male_1-local"))
+                        textToSpeech.setVoice(voice);
+                }
+            }
+        });
 
         ///////////
         guideSpeaker = (GifImageView) findViewById(R.id.wiki_attr_speaking_heroIV);
@@ -53,9 +68,11 @@ public class Wiki_Attraction_Activity extends AppCompatActivity {
             if (running) {
                 gifFromResource.pause();
                 listenBTN.setCompoundDrawablesWithIntrinsicBounds(playImage, null, null, null);
+                textToSpeech.stop();
             } else {
                 gifFromResource.start();
                 listenBTN.setCompoundDrawablesWithIntrinsicBounds(pauseImage, null, null, null);
+                textToSpeech.speak(descrTW.getText().toString(), TextToSpeech.QUEUE_ADD, null);
             }
         });
 
@@ -69,6 +86,14 @@ public class Wiki_Attraction_Activity extends AppCompatActivity {
                     break;
             }
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        gifFromResource.stop();
+        textToSpeech.stop();
+        textToSpeech.shutdown();
     }
 
     private void setInfo(int mDrawable, String title, String description) {
