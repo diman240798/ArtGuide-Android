4064dc88a9a674c33ba3230df24d3c093618a121
Dmitriy <diman240798@yandex.ru>
2019-05-16 22:57:19 +0300

    WikiAttractionViewModel refactoring

--- app/src/main/java/com/dev/nanicki/artguide/fragment/attraction_info/WikiAttractionViewModel.java
+++ app/src/main/java/com/dev/nanicki/artguide/fragment/attraction_info/WikiAttractionViewModel.java
@@ -10,35 +10,22 @@ import android.view.View;
 import com.dev.nanicki.artguide.R;
 
 public class WikiAttractionViewModel extends ViewModel {
-    private final int playImage = R.drawable.listen_play;
-    private final int pauseImage = R.drawable.listen_pause;
 
-    public ObservableInt btnListenImage = new ObservableInt();
-    public ObservableBoolean isPlaying = new ObservableBoolean();
+    private boolean isPlaying = false;
     public MutableLiveData<Boolean> isPlayingLV = new MutableLiveData<>();
 
     public WikiAttractionViewModel() {
-        btnListenImage.set(playImage);
+        super();
+        isPlayingLV.setValue(false);
     }
 
     public void onPlayClicked(View v) {
-        boolean isPlay = !isPlaying.get();
-        onPlayingChanged(isPlay);
+        isPlaying = !isPlaying;
+        isPlayingLV.setValue(isPlaying);
     }
 
     void onStop() {
-        onPlayingChanged(false);
+        isPlayingLV.setValue(false);
     }
 
-    private void onPlayingChanged(boolean current) {
-        isPlaying.set(current);
-        isPlayingLV.setValue(current);
-        changePlayImage();
-    }
-
-    private void changePlayImage() {
-        btnListenImage.set(
-                isPlaying.get() ? pauseImage : playImage
-        );
-    }
 }
--- app/src/main/java/com/dev/nanicki/artguide/util/AttractionBindingAdapters.java
+++ app/src/main/java/com/dev/nanicki/artguide/util/AttractionBindingAdapters.java
@@ -2,11 +2,19 @@ package com.dev.nanicki.artguide.util;
 
 import android.databinding.BindingAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import com.dev.nanicki.artguide.R;
 
 public class AttractionBindingAdapters {
+    private static final int playImage = R.drawable.listen_play;
+    private static final int pauseImage = R.drawable.listen_pause;
+
+
     @BindingAdapter({"android:btnLeftImage"})
-    public static void setImageViewResource(Button button, int resource) {
-        button.setCompoundDrawablesWithIntrinsicBounds(resource, 0, 0, 0);
+    public static void setImageViewResource(Button button, boolean running) {
+        if (running)
+            button.setCompoundDrawablesWithIntrinsicBounds(pauseImage, 0, 0, 0);
+        else
+            button.setCompoundDrawablesWithIntrinsicBounds(playImage, 0, 0, 0);
     }
 }
